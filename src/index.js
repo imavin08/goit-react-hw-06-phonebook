6ac41744547bf09fb41c017a3c7cf1ef949929c6
Imavin08 <92112933+Imavin08@users.noreply.github.com>
2022-08-05 14:29:21 +0200

    fix bug

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import './index.css';
-import { store, persistor } from './redux/store';
+import { store } from './redux/store';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
+// import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
